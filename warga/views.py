f7972158073dc3df19ee5fb7a37e0c1ae00c38d2
bee-farensa <farensanindya04@gmail.com>
2025-10-06 19:16:38 +0700

    pertemuan 1

---
+++ warga/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Warga
+
+class WargaListView(ListView):
+    model = Warga
+
+class WargaDetailView(DetailView):
+    model = Warga
+
+# Create your views here.
